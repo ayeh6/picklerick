92ed17da4e3e4ba5f071186de6d84a02f001498a
Andrew Yeh <ayeh6@ucmerced.edu>
2017-11-30 06:07:18 +0000

    works good enough

--- MST.cpp
+++ MST.cpp
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 using namespace std;
 
-struct Node
+struct ListNode
 {
 	int dest;
 	int weight;
-	struct Node* next;
+	ListNode* next;
 
-	Node(int dest, int weight)
+	ListNode(int dest, int weight)
 	{
 		this->dest = dest;
 		this->weight = weight;
@@ -19,18 +19,18 @@ struct Node
 
 struct List
 {
-	struct Node *head;
+	ListNode *head;
 };
 
 struct Graph
 {
 	int V;
-	struct List* array;
+	List* array;
 
 	Graph(int V)
 	{
 		this->V = V;
-		this->array = (struct List*) malloc (V * sizeof(struct List));
+		this->array = (List*) malloc (V * sizeof(List));
 		for(int i = 0; i < V; i++)
 		{
 			this->array[i].head = NULL;
@@ -38,55 +38,61 @@ struct Graph
 	}
 };
 
-void addEdge(struct Graph* graph, int source, int destination, int weight)
+void addEdge(Graph* graph, int src, int dest, int weight)
 {
-	Node* newNode = new Node(destination, weight);
-	newNode->next = graph->array[source].head;
-	graph->array[source].head = newNode;
-	newNode = new Node(source, weight);
-	newNode->next = graph->array[destination].head;
-	graph->array[destination].head = newNode;
+	ListNode* nNode = new ListNode(dest, weight);
+	nNode->next = graph->array[src].head;
+	graph->array[src].head = nNode;
+	nNode = new ListNode(src, weight);
+	nNode->next = graph->array[dest].head;
+	graph->array[dest].head = nNode;
 }
 
-struct MinHeapNode
+struct GraphNode
 {
 	int v;
 	int key;
 
-	MinHeapNode(int v, int key)
+	GraphNode(int v, int key)
 	{
 		this->v = v;
 		this->key = key;
 	}
 };
 
-struct MinHeap
+struct MinHeapGraph
 {
 	int size;
 	int capacity;
-	int *position;
-	struct MinHeapNode **array;
+	int *pos;
+	GraphNode **array;
 
-	MinHeap(int capacity)
+	MinHeapGraph(int capacity)
 	{
-		this->position = (int *)malloc(capacity * sizeof(int));
+		this->pos = (int *)malloc(capacity * sizeof(int));
 		this->size = 0;
 		this->capacity = capacity;
-		this->array = (struct MinHeapNode**)malloc(capacity * sizeof(struct MinHeapNode*));
+		this->array = (GraphNode**)malloc(capacity * sizeof(GraphNode*));
 	}
 };
 
-void swap(struct MinHeapNode** x, struct MinHeapNode** y)
+void swap(GraphNode** x, GraphNode** y)
 {
-	struct MinHeapNode* temp = *x;
+	GraphNode* temp = *x;
 	*x = *y;
 	*y = temp;
 }
 
-void minHeapify(struct MinHeap* minHeap, int index)
+void minHeapify(MinHeapGraph* minHeap, int index)
 {
-	int minNode = index, left = 2 * index + 1, right = 2 * index + 2;
-	if((right < minHeap->size) && (minHeap->array[right]->key < minHeap->array[minNode]->key)) {
+	int minNode = index;
+	int left;
+	int right;
+	left = 2 * index + 1;
+	right = 2 * index + 2;
+
+	if((right < minHeap->size) && (minHeap->array[right]->key < minHeap->array[minNode]->key))
+	{
 		minNode = right;
 	}
 	if((left < minHeap->size) && (minHeap->array[left]->key < minHeap->array[minNode]->key))
@@ -95,72 +101,72 @@ void minHeapify(struct MinHeap* minHeap, int index)
 	}
 	if(minNode != index)
 	{
-		MinHeapNode *smallestNode = minHeap->array[minNode];
-		MinHeapNode *idxNode = minHeap->array[index];
-		minHeap->position[smallestNode->v] = index;
-		minHeap->position[idxNode->v] = minNode;
+		GraphNode *smllNode = minHeap->array[minNode];
+		GraphNode *idxNode = minHeap->array[index];
+		minHeap->pos[smllNode->v] = index;
+		minHeap->pos[idxNode->v] = minNode;
 		swap(&minHeap->array[minNode], &minHeap->array[index]);
 		minHeapify(minHeap, minNode);
 	}
 }
 
-struct MinHeapNode* extractMin(struct MinHeap* minHeap)
+struct GraphNode* getMin(MinHeapGraph* minHeap)
 {
 	if(minHeap->size == 0)
 	{
 		return NULL;
 	}
-	MinHeapNode* root = minHeap->array[0]; // Store the root node
-	MinHeapNode* lastNode = minHeap->array[minHeap->size - 1];
+	GraphNode* root = minHeap->array[0];
+	GraphNode* lastNode = minHeap->array[minHeap->size - 1];
 	minHeap->array[0] = lastNode;
-	minHeap->position[root->v] = minHeap->size-1;
-	minHeap->position[lastNode->v] = 0;
+	minHeap->pos[root->v] = minHeap->size-1;
+	minHeap->pos[lastNode->v] = 0;
 	--minHeap->size;
 	minHeapify(minHeap, 0);
 	return root;
 }
 
-void DecreaseKey(struct MinHeap* minHeap, int v, int key)
+void DecreaseKey(MinHeapGraph* minHeap, int v, int key)
 {
-	int i = minHeap->position[v];
+	int i = minHeap->pos[v];
 	minHeap->array[i]->key = key;
 	while((minHeap->array[(i - 1) / 2]->key) > (minHeap->array[i]->key))
 	{
-		minHeap->position[minHeap->array[i]->v] = (i-1)/2;
-		minHeap->position[minHeap->array[(i-1)/2]->v] = i;
+		minHeap->pos[minHeap->array[i]->v] = (i-1)/2;
+		minHeap->pos[minHeap->array[(i-1)/2]->v] = i;
 		swap(&minHeap->array[i],  &minHeap->array[(i - 1) / 2]);
 		i = (i - 1) / 2;
 	}
 }
 
-void Prims(struct Graph* graph)
+void Prims(Graph* graph)
 {
 	int V = graph->V;
 	int parents[V], key[V];
- 	MinHeap* minHeap = new MinHeap(V);
+ 	MinHeapGraph* minHeap = new MinHeapGraph(V);
 
 	for(int i = 0; i < V; i++)
 	{
 		parents[i] = -1;
         	key[i] = INT_MAX;
-		minHeap->array[i] = new MinHeapNode(i, key[i]);
-		minHeap->position[i] = i;
+		minHeap->array[i] = new GraphNode(i, key[i]);
+		minHeap->pos[i] = i;
 	}
 
 	key[0] = 0;
-	minHeap->array[0] = new MinHeapNode(0, key[0]);
-	minHeap->position[0] = 0;
+	minHeap->array[0] = new GraphNode(0, key[0]);
+	minHeap->pos[0] = 0;
 	minHeap->size = V;
 
 	while(!(minHeap->size == 0))
 	{
-		MinHeapNode* minHeapNode = extractMin(minHeap);
+		GraphNode* minHeapNode = getMin(minHeap);
 		int u = minHeapNode->v;
-		Node* tempArray = graph->array[u].head;
+		ListNode* tempArray = graph->array[u].head;
 		while (tempArray != NULL)
 		{
 			int v = tempArray->dest;
-			if((minHeap->position[v] < minHeap->size) && (tempArray->weight < key[v]))
+			if((minHeap->pos[v] < minHeap->size) && (tempArray->weight < key[v]))
 			{
 				key[v] = tempArray->weight;
 				parents[v] = u;
